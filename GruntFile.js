817408c113637b1bc2858c3c2726edb3b6fc915f
khali123 <khalid.biq2017@gmail.com>
2022-09-18 14:31:17 +0100

    grun commit1

---
+++ GruntFile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+       
+    });
+
+ 
+
+    grunt.registerTask('run', function () {
+        console.log("i am running")
+        
+    });
+
+    grunt.registerTask('sleep', function () {
+        console.log("i am sleepign")
+
+    });
+
+    grunt.registerTask('all', ['run','sleep']);
+};
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Commad to start grunt project
+<h6>grunt NomTask</h6>
+example nom task run 
+or name task sleep 
+
